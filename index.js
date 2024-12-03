9b4e070081d81b656cca1e42665491ed22a2fc23
fahrizarizky <fahrizarizky6@gmail.com>
2024-12-03 22:06:51 +0700

    first setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/shop_db")
+  .then((res) => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
