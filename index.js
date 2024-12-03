9fecdc1de4773c3501c9cc1bf47f8e529be0b30a
fahrizarizky <fahrizarizky6@gmail.com>
2024-12-03 23:57:51 +0700

    Membuat Restful Index Product

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose
     console.log(err);
   });
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 //Import Model
@@ -20,6 +20,11 @@ const Product = require("./models/products");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
 });
 
 app.listen(3000, () => {
